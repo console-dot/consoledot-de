709b104a73350d1b4ded5efe2346ef6ca57a4544
Shuja-Siddiqui <shuja0094@gmail.com>
2023-12-22 21:18:45 +0500

    text slider

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,15 @@
-import { Footer } from '../Footer';
-import { Navbar } from '../Navbar';
+import { Footer } from "../Footer";
+import { Navbar } from "../Navbar";
+import { TextSlider } from "../TextSlider";
 
 export const Layout = ({ children }) => {
+  const texts = ["Text 1", "Text 2", "Text 3"];
   return (
-    <>
+    <div style={{ maxWidth: "100%", overflow: "hidden" }}>
+      <TextSlider texts={texts} />
       <Navbar />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
---
+++ src/components/TextSlider/TextSLider.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./textslider.css";
+
+export const TextSlider = () => {
+  return (
+    <div style={{ background: "black" }}>
+      <h3 className="words">Website is Under Maintenance</h3>
+    </div>
+  );
+};
